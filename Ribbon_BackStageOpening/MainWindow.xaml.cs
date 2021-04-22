b6a8b687b2342f8e0303983295ba455e6a682558
Elakkiya1730 <70741355+Elakkiya1730@users.noreply.github.com>
2021-04-22 16:56:51 +0530

    Added sample file

---
+++ Ribbon_BackStageOpening/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using Syncfusion.SfSkinManager;
+using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp1.MainWindow;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ribbon.BackStageOpening += new System.ComponentModel.CancelEventHandler(ribbon_BackStageOpening);
+        }
+
+
+        void ribbon_BackStageOpening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (BackStageOpeningCheckBox.IsChecked == true)
+                e.Cancel = true;
+        }
+    }
+}
